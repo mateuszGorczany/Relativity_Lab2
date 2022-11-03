ea59969197c0fb37c8bf6a5d7e03f818cd56ed66
mateuszGorczany <mateusz.g64@interia.pl>
2022-11-03 09:19:32 -0700

    read from env var2 +add failing test

---
+++ Lab2_TestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Lab2_TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- WebAppLab2/Program.cs
+++ WebAppLab2/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var configuration = (IConfiguration)app.Services.GetService(typeof(IConfiguration))!;
+
 app.MapGet("/", () => $"Hello World! Value: {configuration.GetSection("test").Value}");
 
 app.Run();
